9e1e4fef63917e8c8a81866101d2b7215f5e18cc
Ian Oxley <ijoxley@gmail.com>
2013-10-08 10:56:02 +0100

    Added js2-toggle-indent.
    Toggles the indent from 2 to 4, or 4 to 2, depending on the current
    value.

--- ox/javascript.el
+++ ox/javascript.el
@@ -3,3 +3,10 @@
 (defun js2-execute-buffer ()
   (interactive)
   (shell-command (concat "node " (buffer-file-name))))
+
+(defun js2-toggle-indent ()
+	(if (= js2-indent-level 2)
+		(setq js2-indent-level 4)
+	  (setq js2-indent-level 2))
+
+	(message (concat "js-indent-level is " (number-to-string js2-indent-level))))
