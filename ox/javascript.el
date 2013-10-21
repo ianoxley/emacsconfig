a99750d891f0a521c51e73b72d911629e8d2b432
Ian Oxley <ijoxley@gmail.com>
2013-10-21 15:47:28 +0100

    js2-toggle-indent function added.
    Some JavaScript libraries prefer 4 spaces for tabs, some prefer 2. This
    function allows you to toggle between the two.

--- ox/javascript.el
+++ ox/javascript.el
@@ -1,12 +1,19 @@
 ;; javascript.el
 
+(require 'js2-mode)
+
 (defun js2-execute-buffer ()
   (interactive)
   (shell-command (concat "node " (buffer-file-name))))
 
 (defun js2-toggle-indent ()
-	(if (= js2-indent-level 2)
-		(setq js2-indent-level 4)
-	  (setq js2-indent-level 2))
-
-	(message (concat "js-indent-level is " (number-to-string js2-indent-level))))
+  (interactive)
+  (if (= js2-indent-level 2)
+	(setq js2-indent-level 4)
+	(setq js2-indent-level 4)
+	(setq js2-basic-offset 4)
+  (setq js2-indent-level 2)
+  (setq js2-indent-level 2)
+  (setq js2-basic-offset 2))
+  
+  (message (concat "js-indent-level set to " (number-to-string js2-indent-level))))
