7b687a64a98c574b6eff3150cfff056ce2c085c8
Ian Oxley <ijoxley@gmail.com>
2015-01-30 11:04:57 +0000

    set-alist and evil-emacs-state-modes
    set-alist installed if package is missing.
    
    evil mode disabled when loading question list, and inbox mode.

--- ox/sx.el
+++ ox/sx.el
@@ -1,8 +1,16 @@
 ;; sx config
 
+(unless (package-installed-p 'let-alist)
+  (package-install 'let-alist))
+
 (add-to-list 'load-path
 			 (convert-standard-filename "~/emacsconfig/lisp/sx"))
-(prefer-coding-system 'utf-8)
-(require 'sx-load)
 
+(prefer-coding-system 'utf-8)
+
+(require 'sx-load)
 (global-set-key (kbd "C-c x") 'sx-tab-frontpage)
+
+(when (boundp 'evil-emacs-state-modes)
+  (add-to-list 'evil-emacs-state-modes 'sx-question-list-mode)
+  (add-to-list 'evil-emacs-state-modes 'sx-inbox-mode))
