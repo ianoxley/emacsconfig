7d3a695b5492a5023ff9cb9a9e235cdf0fdd30c8
Ian Oxley <ijoxley@gmail.com>
2015-02-19 10:25:51 +0000

    sx installed via package repository.

--- ox/sx.el
+++ ox/sx.el
@@ -3,14 +3,10 @@
 (unless (package-installed-p 'let-alist)
   (package-install 'let-alist))
 
-(add-to-list 'load-path
-			 (convert-standard-filename "~/emacsconfig/lisp/sx"))
+(unless (package-installed-p 'sx)
+  (package-install 'sx))
 
 (prefer-coding-system 'utf-8)
 
 (require 'sx-load)
 (global-set-key (kbd "C-c x") 'sx-tab-frontpage)
-
-(when (boundp 'evil-emacs-state-modes)
-  (add-to-list 'evil-emacs-state-modes 'sx-question-list-mode)
-  (add-to-list 'evil-emacs-state-modes 'sx-inbox-mode))
